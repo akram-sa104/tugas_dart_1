3c8a9a9ed816224dd66192e62b1c323f6ccf2a01
akram-sa104 <akramsuryaalfarras@gmail.com>
2025-10-17 19:27:33 +0700

    update

---
+++ lib/control flow/branchesIf.dart
@@ -0,0 +1,20 @@
+void main() {
+if (showWidget()) {
+myWidget();
+} else if (showOtherWidget()) {
+myOtherWidget();
+} else {
+normalWidget();
+}
+}
+bool showWidget() {
+return true;
+}
+bool showOtherWidget() => false;
+void myWidget() {
+print('This is my widget');
+}
+void myOtherWidget() {
+return print('This is my other widget');
+}
+void normalWidget() => print('This is normal widget');
